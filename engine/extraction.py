673a3114b0fe2b9a43d3ceab82ddc9a9deb11f4f
Kevin Le Nguyen <s88877@stud.htw-dresden.de>
2026-07-11 23:41:10 +0200

    feat(engine): add method to extract data from user input

--- engine/extraction.py
+++ engine/extraction.py
@@ -10,7 +10,11 @@ import os
 
 load_dotenv()
 
-print("OPENAI_API_KEY:", os.getenv("OPENAI_API_KEY"))
+client = OpenAI(
+    base_url="https://models.github.ai/inference",
+    api_key=os.getenv("GITHUB_TOKEN")
+)
+
 class SavingGoal(BaseModel):
     """
     A Pydantic model representing a saving goal with a date and amount.
@@ -20,3 +24,31 @@ class SavingGoal(BaseModel):
     """
     target_date: date = Field(..., description="The date of the saving goal in YYYY-MM-DD format.")
     target_amount: float = Field(..., description="The amount of the saving goal.")
+
+def extract_saving_goal(user_input: str) -> SavingGoal:
+    """
+    Extracts the saving goal information from the user input using the OpenAI API and returns a SavingGoal object.
+    Parameters:
+        user_input (str): The user input containing the saving goal information.
+    Returns:
+        SavingGoal: An instance of the SavingGoal model containing the extracted target date and amount.
+    """
+    client_call = client.beta.chat.completions.parse(
+        model="gpt-4o-mini",
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a helpful assistant that extracts saving goal information from user input."
+             },
+            {
+                "role": "user",
+                "content": user_input
+            }
+        ],   
+        response_format=SavingGoal
+        )
+    return client_call.choices[0].message.parsed
+
+if __name__ == "__main__":
+    result = extract_saving_goal("I want to save 5000 Euro by December 31, 2026.")
+    print(result)
